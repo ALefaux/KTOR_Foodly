cf601be290aa09ed3da8711cc84aabcfd0f70e09
Axel Lefaux <axel.lefaux@outlook.fr>
2021-12-09 13:38:48 +0100

    Add postgres dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val hikari_version: String by project
+val postgresql_version: String by project
 
 plugins {
     application
@@ -23,4 +26,9 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // Exposed & Postgresql
+    implementation("org.jetbrains.exposed:exposed:$exposed_version")
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+    implementation("org.postgresql:postgresql:$postgresql_version")
 }
