b9816868335fd0e50ba001021c7caeae63a55cfa
Axel Lefaux <axel.lefaux@outlook.fr>
2021-12-09 13:26:02 +0100

    Init project

---
+++ src/main/kotlin/fr/alefaux/Application.kt
@@ -0,0 +1,12 @@
+package fr.alefaux
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import fr.alefaux.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureMonitoring()
+        configureRouting()
+    }.start(wait = true)
+}
